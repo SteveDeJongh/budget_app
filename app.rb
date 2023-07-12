588c16ea289e335ca3f83854d2a583d0badd1e87
Steve De Jongh <de.jongh.steve@gmail.com>
2023-07-12 10:57:51 -0700

    listing expenses

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 
 require 'sinatra'
 require 'tilt/erubis'
+require 'pry'
 
 configure do
   enable :sessions
@@ -24,5 +25,14 @@ after do
 end
 
 get '/' do
+  @expenses = @storage.all_expenses
   erb :home, layout: :layout
+  # binding.pry
+end
+
+post '/addexpense' do
+  @data = [params[:payee].strip, params[:category].strip, params[:date].strip, params[:amount].strip]
+  @storage.addexpense(@data[0], @data[1], @data[2], @data[3])
+  session[:success] = 'Transaction has been added.'
+  redirect '/'  
 end
--- database_persistence.rb
+++ database_persistence.rb
@@ -10,4 +10,39 @@ class Databasepersistence
   def disconnect
     @db.close
   end
+
+  def query(statement, *params)
+    @db.exec_params(statement, params)
+  end
+
+  def all_expenses
+    sql = <<~SQL
+      SELECT * FROM expenses;
+    SQL
+
+    result = query(sql)
+
+    result.map do |tuple|
+      tuple_to_expense_hash(tuple)
+    end
+  end
+
+  def addexpense(payee, category, craeted_on, amount)
+    sql = <<~SQL
+      INSERT INTO expenses (payee, category, created_on, amount)
+      VALUES ($1, $2, $3, $4);
+    SQL
+    
+    query(sql, payee, category, craeted_on, amount)
+  end
+
+  def tuple_to_expense_hash(tuple)
+    { id: tuple["id"].to_i,
+      payee: tuple["payee"],
+      category: tuple["cagtegory"],
+      date: tuple["created_on"],
+      amount: tuple["amount"] }
+  end
+
+
 end
