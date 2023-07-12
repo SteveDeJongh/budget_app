2da3e28738a1826d83fa35e0705742498196a55c
Steve De Jongh <de.jongh.steve@gmail.com>
2023-07-12 15:56:16 -0700

    editing an expense

--- app.rb
+++ app.rb
@@ -26,36 +26,25 @@ after do
 end
 
 def expense_input_validation(data)
-  message = check_payee(data[0])
-  message = check_category(data[1])
-  message = check_date(data[2])
-  message = check_amount(data[3])
+  message = if !(1..100).cover?(data[0].size)
+              "Payee name must be between 1 and 100 characters."
+            elsif !(1..100).cover?(data[1].size)
+              "Category must be between 1 and 100 characters."
+            elsif !data[2].match(/\b\d\d\d\d-\d\d-\d\d\b/)
+              "Date must be written out as YYYY-MM-DD."
+            elsif data[3].to_i.to_s != data[3] || data[3].to_i > 9999.99
+              "Amount must be a number less than $10,000."
+            end
 
   message ? message : nil
 end
 
-def check_payee(payee)
-  if !(1..100).cover?(payee.size)
-    "Payee name must be between 1 and 100 characters."
-  end
-end
+def find_expense(id)
+  expense = @storage.find_expense(id)
+  return expense if expense
 
-def check_category(category)
-  if !(1..100).cover?(category.size)
-    "Category must be between 1 and 100 characters."
-  end
-end
-
-def check_date(date)
-  if !date.match(/\b\d\d\d\d-\d\d-\d\d\b/)
-    "Date must be written out as YYYY-MM-DD."
-  end
-end
-
-def check_amount(amount)
-  if amount.to_i != amount || amount.to_i > 100000
-    "Amount must be a number less than 100000."
-  end
+  session[:error] = "Expense not found."
+  redirect '/'
 end
 
 get '/' do
@@ -83,4 +72,31 @@ end
 
 get '/addexpense' do
   erb :addexpense, layout: :layout
+end
+
+get '/expense/:id' do
+  @expense = find_expense(params[:id])
+  erb :viewexpense, layout: :layout
+end
+
+get '/editexpense/:id' do
+  @expense = find_expense(params[:id])
+  erb :editexpense, layout: :layout
+end
+
+post '/editexpense/:id' do
+  @data = [params[:payee].strip, params[:category].strip, params[:date].strip, params[:amount].strip, params[:id].to_i]
+  error = expense_input_validation(@data)
+  if error
+    session[:error] = error
+    redirect "/editexpense/#{params[:id]}"
+  else
+    @storage.editexpense(@data[0], @data[1], @data[2], @data[3], @data[4])
+    session[:success] = 'Transaciton has been edited.'
+    redirect "/expense/#{params[:id]}"
+  end
+end
+
+post '/deleteexpense/:id' do
+  # to do...
 end
--- database_persistence.rb
+++ database_persistence.rb
@@ -28,13 +28,34 @@ class Databasepersistence
     end
   end
 
-  def addexpense(payee, category, craeted_on, amount)
+  def addexpense(payee, category, created_on, amount)
     sql = <<~SQL
       INSERT INTO expenses (payee, category, created_on, amount)
       VALUES ($1, $2, $3, $4);
     SQL
     
-    query(sql, payee, category, craeted_on, amount)
+    query(sql, payee, category, created_on, amount)
+  end
+
+  def find_expense(id)
+    sql = <<~SQL 
+      SELECT * FROM expenses
+      WHERE id = $1;
+    SQL
+
+    result = query(sql, id)
+
+    tuple_to_expense_hash(result.first)
+  end
+
+  def editexpense(payee, category, created_on, amount, id)
+    sql = <<~SQL
+      UPDATE expenses 
+      SET payee = $1, category = $2, created_on = $3, amount = $4
+      WHERE id = $5;
+    SQL
+    
+    query(sql, payee, category, created_on, amount, id)
   end
 
   def tuple_to_expense_hash(tuple)
